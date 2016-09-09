a1efe94cc7f71b1dfdaa079969a22fb510c3d91c
Frej Sundqvist <frej.sundqvist@live.se>
2016-09-09 21:44:21 +0200

    'echo'

--- client.c
+++ client.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void err_sys(char* mes){
+	perror(mes);
+	exit(EXIT_FAILURE);
+}
 
 int main(){
+	int sockfd_client;
+	struct sockaddr_in echoServAddr;
+	echoServAddr.sin_family = AF_INET;
+	echoServAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	echoServAddr.sin_port = htons(5100);
 
 
+	if((sockfd_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		err_sys("Socket create error");
+
+	if(connect(sockfd_client, (struct sockaddr*)&echoServAddr, sizeof(echoServAddr)) < 0)
+		err_sys("Socket connect error");
+
+	char* echoString = "Hola!\n";
+	int echoStrLen = strlen(echoString);
+
+	if(send(sockfd_client, echoString, echoStrLen, 0) != echoStrLen)
+		err_sys("Socket send error, different number of bytes than expected");
+
+	if(close(sockfd_client) < 0)
+		err_sys("Socket close error");
 }
--- server.c
+++ server.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+void err_sys(char* mes);
 
 int main(){
+	int sockfd_listen, sockfd_client;
+	int clntLen;
+	char recvbuf[10];
+	struct sockaddr_in addrport, echoClntAddr;
+	addrport.sin_family = AF_INET;
+	addrport.sin_port = htons(5100);
+	addrport.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if((sockfd_listen = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+		err_sys("Socket open error");
+
+	if(bind(sockfd_listen, (struct sockaddr*)&addrport, sizeof(addrport)) < 0)
+		err_sys("Socket bind error");
+
+	if(listen(sockfd_listen, 10) < 0)
+		err_sys("Socket listen error");
 
 
+	/* Repeat: */
+//	while(1){
+		clntLen = sizeof(echoClntAddr);
+		if((sockfd_client = accept(sockfd_listen, (struct sockaddr*)&echoClntAddr, &clntLen)) < 0)
+			err_sys("Socket accept error");
 
+		/* Accepted */
+		if(recv(sockfd_client, recvbuf, 10, 0) < 0)
+			err_sys("Socket recieve error");
 
+		fputs(recvbuf, stdout);
+
+//	}
+
+	if(close(sockfd_listen) < 0)
+		err_sys("Socket close error");
+}
+
+void err_sys(char* mes){
+	perror(mes);
+	exit(EXIT_FAILURE);
 }
